47778f5467b129047f0e85a35370e4e9b8a4c5ac
steve nene <stevekid705@gmail.com>
2024-10-04 12:32:39 +0300

    added a white sace

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -13,3 +13,35 @@ export function taskFirst() {
   
     return combination;
   }
+<<<<<<< Tabnine <<<<<<<
+  /**//+
+   * This function returns a string that starts with a constant declaration.//+
+   *//+
+   * @returns {string} - The string starts with 'I prefer const when I can.'//+
+   *///+
+  export function taskFirst() {//+
+    const task = 'I prefer const when I can.';//+
+    return task;//+
+  }//+
+  //+
+  /**//+
+   * This function returns a string that can be appended to the end of another string.//+
+   *//+
+   * @returns {string} - The string ' is okay'//+
+   *///+
+  export function getLast() {//+
+    return ' is okay';//+
+  }//+
+  //+
+  /**//+
+   * This function combines two strings using 'let'.//+
+   *//+
+   * @returns {string} - The combined string 'But sometimes let is okay'//+
+   *///+
+  export function taskNext() {//+
+    let combination = 'But sometimes let';//+
+    combination += getLast();//+
+  //+
+    return combination;//+
+  }//+
+>>>>>>> Tabnine >>>>>>>// {"conversationId":"cf53f27b-6eb0-433c-86e3-cd49dadee8d6","source":"instruct"}
