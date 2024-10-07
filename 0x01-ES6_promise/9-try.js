3b82ccaf5e6c83b8f572275ed1187daa9ec3e4a3
steve nene <stevekid705@gmail.com>
2024-10-07 12:09:47 +0300

    romises files done

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,11 @@
+export default function guardrail(mathFunction) {
+    const queue = [];
+    try {
+      const result = mathFunction();
+      queue.push(result, 'Guardrail was processed');
+    } catch (err) {
+      queue.push(err.toString(), 'Guardrail was processed');
+    }
+    return queue;
+  }
+  
