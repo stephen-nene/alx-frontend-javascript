f65b9794a491db146695052e40d5112b432bb5b5
steve nene <stevekid705@gmail.com>
2024-10-09 13:26:41 +0300

    fixed code 0

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,7 +1,6 @@
 export default function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-      resolve('Example');
-      reject(new Error('Error message here'));
-    });
-  }
-  
+  return new Promise((resolve, reject) => {
+    resolve('Example');
+    reject(new Error('Error message here'));
+  });
+}
