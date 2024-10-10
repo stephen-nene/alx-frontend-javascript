18de2e1279d8a003c132b6dfdc51fa647f3ba361
steve nene <stevekid705@gmail.com>
2024-10-10 11:50:57 +0300

    did till clean set

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,15 @@
+export default function cleanSet(set, startString) {
+  if (!startString || typeof startString !== 'string') {
+    return '';
+  }
+
+  const result = [];
+
+  for (const value of set) {
+    if (value && value.startsWith(startString)) {
+      result.push(value.slice(startString.length));
+    }
+  }
+
+  return result.join('-');
+}
